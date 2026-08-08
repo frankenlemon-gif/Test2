e804ec657abddea3aac7d2b40c910a37f7430afc
frankenlemon-gif <frankenlemon@gmail.com>
2026-08-08 12:58:29 +0500

    Update LauncherActivity.java

--- app/src/main/java/fake/launcher/LauncherActivity.java
+++ app/src/main/java/fake/launcher/LauncherActivity.java
@@ -1,6 +1,7 @@
 package fake.launcher;
 
 import android.app.Activity;
+import android.app.KeyguardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -35,10 +36,23 @@ public class LauncherActivity extends Activity {
     private LauncherApps launcherApps;
     private List<LauncherActivityInfo> apps;
     private LauncherApps.Callback callback;
+    private KeyguardManager keyguardManager;
+    private boolean wasLockedAndFinished = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+
+        if (keyguardManager != null && keyguardManager.isKeyguardLocked()) {
+            wasLockedAndFinished = true;
+            setShowWhenLocked(false);
+            return;
+        } else {
+            wasLockedAndFinished = false;
+            setShowWhenLocked(true);
+        }
 
         launcherApps = (LauncherApps) getSystemService(Context.LAUNCHER_APPS_SERVICE);
         
@@ -153,6 +167,16 @@ public class LauncherActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        if (keyguardManager != null && keyguardManager.isKeyguardLocked()) {
+            wasLockedAndFinished = true;
+            setShowWhenLocked(false);
+            return;
+        } else {
+            wasLockedAndFinished = false;
+            setShowWhenLocked(true);
+        }
+
         getWindow().getDecorView().setSystemUiVisibility(
 			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -162,6 +186,16 @@ public class LauncherActivity extends Activity {
 			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         );
         loadApps();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (wasLockedAndFinished) {
+			wasLockedAndFinished = false;
+            setShowWhenLocked(false);
+            finish();
+        }
     }
 
     private void loadApps() {
