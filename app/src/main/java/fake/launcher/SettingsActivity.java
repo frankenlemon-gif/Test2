6cabd6cc9d13d8a815a5e3445c7e771503e27427
frankenlemon-gif <frankenlemon@gmail.com>
2026-08-08 10:47:11 +0500

    Update SettingsActivity.java

--- app/src/main/java/fake/launcher/SettingsActivity.java
+++ app/src/main/java/fake/launcher/SettingsActivity.java
@@ -161,6 +161,19 @@ public class SettingsActivity extends Activity {
         }
     }
 
+       @Override
+    protected void onResume() {
+        super.onResume();
+        getWindow().getDecorView().setSystemUiVisibility(
+			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );        
+    }
+
     private class SettingsAdapter extends ArrayAdapter<LauncherActivityInfo> {
         public SettingsAdapter() {
             super(SettingsActivity.this, android.R.layout.simple_list_item_multiple_choice, allApps);
