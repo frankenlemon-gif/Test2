d61d3c0ebfb5ed92e5b916c221cdb0ac0f0b81df
frankenlemon-gif <frankenlemon@gmail.com>
2026-08-08 07:03:28 +0500

    Update LauncherActivity.java

--- app/src/main/java/fake/launcher/LauncherActivity.java
+++ app/src/main/java/fake/launcher/LauncherActivity.java
@@ -8,7 +8,6 @@ import android.content.pm.LauncherActivityInfo;
 import android.content.pm.LauncherApps;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Process;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.PreferenceManager;
@@ -29,22 +28,20 @@ public class LauncherActivity extends Activity {
 
     private GridView gridView;
     private LauncherApps launcherApps;
-    private UserManager userManager;
     private List<LauncherActivityInfo> apps;
+    private LauncherApps.Callback callback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         launcherApps = (LauncherApps) getSystemService(Context.LAUNCHER_APPS_SERVICE);
-        userManager = (UserManager) getSystemService(Context.USER_SERVICE);
-
         gridView = new GridView(this);
         gridView.setNumColumns(4);
         gridView.setBackgroundColor(Color.TRANSPARENT);
 
         gridView.setOnItemLongClickListener((parent, view, position, id) -> {
-            startActivity(new Intent(LauncherActivity.this, SettingsActivity.class));
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         });
 
@@ -53,7 +50,27 @@ public class LauncherActivity extends Activity {
             launcherApps.startMainActivity(info.getComponentName(), info.getUser(), null, null);
         });
 
+        callback = new LauncherApps.Callback() {
+            @Override
+            public void onPackageAdded(String packageName, UserHandle user) { loadApps(); }
+            @Override
+            public void onPackageRemoved(String packageName, UserHandle user) { loadApps(); }
+            @Override
+            public void onPackageChanged(String packageName, UserHandle user) { loadApps(); }
+            @Override
+            public void onPackagesAvailable(String[] packageNames, UserHandle user, boolean replacing) { loadApps(); }
+            @Override
+            public void onPackagesUnavailable(String[] packageNames, UserHandle user, boolean replacing) { loadApps(); }
+        };
+
+        launcherApps.registerCallback(callback);
         setContentView(gridView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        launcherApps.unregisterCallback(callback);
     }
 
     @Override
@@ -65,45 +82,39 @@ public class LauncherActivity extends Activity {
     private void loadApps() {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         Set<String> hidden = prefs.getStringSet("hidden", new HashSet<>());
-
         apps = new ArrayList<>();
-        for (UserHandle user : userManager.getUserProfiles()) {
-            List<LauncherActivityInfo> activityList = launcherApps.getActivityList(null, user);
-            for (LauncherActivityInfo info : activityList) {
+        UserManager um = (UserManager) getSystemService(Context.USER_SERVICE);
+
+        for (UserHandle user : um.getUserProfiles()) {
+            List<LauncherActivityInfo> list = launcherApps.getActivityList(null, user);
+            for (LauncherActivityInfo info : list) {
                 if (!hidden.contains(info.getApplicationInfo().packageName)) {
                     apps.add(info);
                 }
             }
         }
-        gridView.setAdapter(new AppAdapter());
+        runOnUiThread(() -> gridView.setAdapter(new AppAdapter()));
     }
 
     private class AppAdapter extends BaseAdapter {
-        @Override
-        public int getCount() { return apps.size(); }
-        @Override
-        public Object getItem(int position) { return apps.get(position); }
-        @Override
-        public long getItemId(int position) { return position; }
+        @Override public int getCount() { return apps.size(); }
+        @Override public Object getItem(int position) { return apps.get(position); }
+        @Override public long getItemId(int position) { return position; }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             LinearLayout layout = new LinearLayout(LauncherActivity.this);
             layout.setOrientation(LinearLayout.VERTICAL);
             layout.setGravity(Gravity.CENTER);
-            layout.setPadding(20, 20, 20, 20);
-
-            LauncherActivityInfo info = apps.get(position);
-
+            
             ImageView icon = new ImageView(LauncherActivity.this);
             icon.setLayoutParams(new LinearLayout.LayoutParams(150, 150));
-            icon.setImageDrawable(info.getIcon(0));
+            icon.setImageDrawable(apps.get(position).getIcon(0));
 
             TextView text = new TextView(LauncherActivity.this);
-            text.setText(info.getLabel());
+            text.setText(apps.get(position).getLabel());
             text.setTextColor(Color.WHITE);
-            text.setGravity(Gravity.CENTER);
-
+            
             layout.addView(icon);
             layout.addView(text);
             return layout;
